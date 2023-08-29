c90c1a3da8865d84be66a9e3ce90ea321a778f9b
Disha <dishatalreja1202@gmail.com>
2023-08-29 16:45:44 +0530

    Implemented slash command(#85ztvq1pt)

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.event('app_home_opened', ({event, say, client}) => {
     // console.log(event)
     if(event.tab === 'home' && !event.view) {
         // send welcome message
-        say(`Hi <@${event.user}> /n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices dui sapien eget mi proin. Aliquet lectus proin nibh nisl condimentum. Vitae tempus quam pellentesque nec nam. Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar. Nunc sed augue lacus viverra vitae congue. Malesuada fames ac turpis egestas maecenas pharetra convallis posuere morbi. Duis ut diam quam nulla porttitor massa id neque aliquam. Amet facilisis magna etiam tempor. Egestas sed sed risus pretium quam vulputate dignissim suspendisse in. Senectus et netus et malesuada fames ac. Sit amet consectetur adipiscing elit duis. Sagittis aliquam malesuada bibendum arcu. Pellentesque nec nam aliquam sem et tortor consequat id porta.`);
+        say(`Hi <@${event.user}> \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices dui sapien eget mi proin. Aliquet lectus proin nibh nisl condimentum. Vitae tempus quam pellentesque nec nam. Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar. Nunc sed augue lacus viverra vitae congue. Malesuada fames ac turpis egestas maecenas pharetra convallis posuere morbi. Duis ut diam quam nulla porttitor massa id neque aliquam. Amet facilisis magna etiam tempor. Egestas sed sed risus pretium quam vulputate dignissim suspendisse in. Senectus et netus et malesuada fames ac. Sit amet consectetur adipiscing elit duis. Sagittis aliquam malesuada bibendum arcu. Pellentesque nec nam aliquam sem et tortor consequat id porta.`);
         // console.log(event.user);
     }
     if(event.tab === "home") {
@@ -82,17 +82,96 @@ app.event('app_home_opened', ({event, say, client}) => {
                 }
             });
 
-            app.action('activity', async ({body, ack, client}) => {
-                await ack();
-                // access the value of the selected option 
-                console.log(Object.values(body.view.state.values)[0].activity.selected_option.value);
-                client.chat.postMessage({
-                    channel: body.user.id,
-                    text: "Text message https://youtu.be/EjO6qH9rCOg?si=PRiou71iFLugFbyZ"
-                })
-            });
         } catch (error) {
             console.log(error);
         }
     }  
+});
+
+app.action('activity', async ({body, ack, client}) => {
+    await ack();
+    // access the value of the selected option 
+    // console.log(Object.values(body.view.state.values)[0].activity.selected_option.value);
+    client.chat.postMessage({
+        channel: body.user.id,
+        text: "Text message https://youtu.be/EjO6qH9rCOg?si=PRiou71iFLugFbyZ"
+    })
+});
+
+app.command('/activity', async ({body, ack, client}) => {
+    await ack();
+    client.chat.postMessage({
+        channel: body.user_id,
+    
+            blocks: [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Select an activity.",
+                        "emoji": true
+                    }
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Short break",
+                                "emoji": true
+                            },
+                            "value": "click_me_123",
+                            "action_id": "shortBreak"
+                        },
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Take a deep breath",
+                                "emoji": true
+                            },
+                            "value": "click_me_13",
+                            "action_id": "deepBreath"
+                        },
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Desk yoga",
+                                "emoji": true
+                            },
+                            "value": "click_me_13",
+                            "action_id": "deskYoga"
+                        }
+                    ]
+                }
+            ]
+    })
+
+});
+
+app.action('shortBreak', async ({body, ack, client}) => {
+    await ack();
+    client.chat.postMessage({
+        channel: body.user.id,
+        text: "Text message https://youtu.be/EjO6qH9rCOg?si=PRiou71iFLugFbyZ"
+    })
+});
+
+app.action('deepBreath', async ({body, ack, client}) => {
+    await ack();
+    client.chat.postMessage({
+        channel: body.user.id,
+        text: "Text message https://youtu.be/EjO6qH9rCOg?si=PRiou71iFLugFbyZ"
+    })
+});
+
+app.action('deskYoga', async ({body, ack, client}) => {
+    await ack();
+    client.chat.postMessage({
+        channel: body.user.id,
+        text: "Text message https://youtu.be/EjO6qH9rCOg?si=PRiou71iFLugFbyZ"
+    })
 });
